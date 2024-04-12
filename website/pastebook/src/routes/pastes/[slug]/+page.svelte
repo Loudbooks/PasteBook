fc594075ecbce13cbfeb151e5e26c8b021e0e984
Loudbook <loudbooked@gmail.com>
2024-04-11 17:40:04 -0700

    CSS changes

--- website/pastebook/src/routes/pastes/[slug]/+page.svelte
+++ website/pastebook/src/routes/pastes/[slug]/+page.svelte
@@ -52,8 +52,7 @@
     <created>
         {timeSinceStr}
     </created>
-    <br>
-    <br>
+    <break></break>
     <p>
         {content}
     </p>
@@ -62,26 +61,70 @@
 <style lang="scss">
     name {
       display: block;
-      font-size: 5vw;
+      font-size: max(5vw, 30px);
       font-family: Gabarito, sans-serif;
       font-weight: 900;
       text-align: center;
-      width: 100%;
-      white-space: nowrap;
+      width: 100vw;
+      white-space: wrap;
+
+      animation: fadeIn ease 0.7s;
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
+    }
+
+    break {
+      display: block;
+      color: black;
+      background-color: grey;
+      height: 3px;
+      border-radius: 10px;
+      width: 90%;
+      align-content: center;
+      margin: 15px auto auto;
+      opacity: 0;
+
+      animation: fadeIn ease 0.7s;
+      animation-delay: 0.2s;
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
     }
 
     created {
       display: block;
-      font-size: 1.5vw;
+      font-size: max(1.5vw, 15px);
       font-family: Gabarito, sans-serif;
       text-align: center;
       font-weight: 200;
       color:gray;
+      opacity: 0;
+
+      animation: fadeIn ease 0.7s;
+      animation-delay: 0.1s;
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
     }
 
     p {
       font-size: 15 px;
       white-space: pre;
       font-family: "JetBrains Mono", monospace;
+      padding: 20px;
+      margin: 0;
+      opacity: 0;
+
+      animation: fadeIn ease 0.7s;
+      animation-delay: 0.3s;
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
+    }
+
+    @keyframes fadeIn {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
     }
 </style>

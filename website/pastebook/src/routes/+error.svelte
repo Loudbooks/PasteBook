5cf6ca17c61e0a6c21a81842695a08e87e999869
Loudbook <loudbooked@gmail.com>
2024-04-12 23:03:38 -0700

    404

---
+++ website/pastebook/src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<error>
+    <h1>{$page.status}</h1>
+    <p>{$page.error.message}</p>
+</error>
+
+<style lang="scss">
+    error {
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+      justify-content: center;
+
+      h1 {
+        text-align: center;
+        font-size: 30vw;
+        font-family: Gabarito, sans-serif;
+        font-weight: 800;
+        line-height: 25vw;
+        margin: 0;
+      }
+
+      p {
+        text-align: center;
+        font-size: 5vw;
+        font-family: Gabarito, sans-serif;
+        color: gray;
+        font-weight: 500;
+        margin: 0;
+      }
+    }
+</style>
--- website/pastebook/src/routes/pastes/[slug]/+page.server.ts
+++ website/pastebook/src/routes/pastes/[slug]/+page.server.ts
@@ -1,7 +1,15 @@
+import {error} from "@sveltejs/kit";
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
     let path = params.slug
     let { created, content, title} = await fetch("https://pastebook.dev/get/" + path).then(res => res.json()).catch(() => "");
+
+    if (content === undefined) {
+        error(404, {
+            message: 'Content Not Found'
+        });
+    }
 
     return {
         created: created,

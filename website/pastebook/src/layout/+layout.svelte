8c17be08d2c104f828347d7a8c08e7e180c3ac3e
Loudbook <loudbooked@gmail.com>
2024-04-18 09:30:47 -0400

    SEO Data

---
+++ website/pastebook/src/layout/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+import {MetaTags} from "svelte-meta-tags";
+</script>
+
+<MetaTags
+    title="PasteBook"
+    description="Simplistic pastebin that supports automated scanning of logs and reports."
+    openGraph={{
+        title: "PasteBook",
+        description: "Simplistic pastebin",
+        url: "https://pastebook.dev",
+    }}
+/>

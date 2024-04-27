856583f417541453b56fa0fab0202d0fa01ca7b2
Loudbook <loudbooked@gmail.com>
2024-04-26 21:50:17 -0400

    About animation

--- website/pastebook/src/routes/about/+page.svelte
+++ website/pastebook/src/routes/about/+page.svelte
@@ -22,6 +22,10 @@
 </about>
 
 <style lang="scss">
+  :root {
+    --animation: ease;
+  }
+  
   about {
     display: flex;
     flex-direction: column;
@@ -29,7 +33,7 @@
     align-items: center;
     height: 100vh;
     background-color: #eeeeee;
-    transition: all 0.5s ease;
+    transition: all 0.5s var(--animation);
 
     :global(.dark-mode) & {
       background-color: #1a1a1a;
@@ -50,6 +54,8 @@
         font-size: 50px;
         height: 50px;
       }
+
+      animation: fadeUp 1s var(--animation);
     }
 
     .author {
@@ -57,8 +63,9 @@
       font-size: 20px;
       margin-top: 0;
       color: #333333;
+      opacity: 0;
 
-      transition: all 0.5s ease;
+      transition: all 0.5s var(--animation);
 
       :global(.dark-mode) & {
         color: lightgray;
@@ -67,6 +74,10 @@
       @media (max-width: 768px) {
         font-size: 13px;
       }
+
+      animation: fadeUp 1s var(--animation);
+      animation-delay: 0.1s;
+      animation-fill-mode: forwards;
     }
 
     .description {
@@ -79,6 +90,11 @@
       padding-right: 10px;
       color: #333333;
       transition: all 0.5s ease;
+      opacity: 0;
+
+      animation: fadeUp 1s var(--animation);
+      animation-delay: 0.2s;
+      animation-fill-mode: forwards;
 
       :global(.dark-mode) & {
         color: lightgray;
@@ -87,6 +103,13 @@
       @media (max-width: 768px) {
         font-size: 15px;
       }
+    }
+
+    buttons {
+      opacity: 0;
+      animation: fadeUp 1s var(--animation);
+      animation-delay: 0.3s;
+      animation-fill-mode: forwards;
     }
 
     dot {
@@ -124,5 +147,15 @@
       }
     }
 
+    @keyframes fadeUp {
+      from {
+        opacity: 0;
+        transform: translateY(25px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
   }
 </style>

3b29a75eda3d8189cb7bab54875e956b0ec09aac
Loudbook <loudbooked@gmail.com>
2024-05-09 15:38:20 -0400

    Cors

--- src/main/kotlin/dev/loudbook/pastebook/controllers/GetController.kt
+++ src/main/kotlin/dev/loudbook/pastebook/controllers/GetController.kt
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody
+import java.io.OutputStream
 
 @RestController
 class GetController {
@@ -19,20 +21,36 @@ class GetController {
     private val bucket: Bucket = BucketUtils.getBucketPerSeconds(4)
 
     @GetMapping("/get/{id}")
-    fun get(@PathVariable id: String): ResponseEntity<String> {
+    fun get(@PathVariable id: String): ResponseEntity<StreamingResponseBody> {
         if (!bucket.tryConsume(1)) {
-            return ResponseEntity.status(429).body("Rate limit exceeded")
+            val responseBody = StreamingResponseBody { outputStream: OutputStream ->
+                outputStream.write("Rate limit exceeded".toByteArray())
+            }
+
+            return ResponseEntity.status(429).body(responseBody)
         }
 
         val paste = pasteRepository.findById(id).orElse(null) ?: return ResponseEntity.notFound().build()
 
         val headers = HttpHeaders()
-        headers.add("Access-Control-Allow-Origin", "*")
         headers.add("title", paste.title)
         headers.add("reportBook", paste.reportBook.toString())
         headers.add("created", paste.created.toString())
         headers.add("wrap", paste.wrap.toString())
 
-        return ResponseEntity.ok().headers(headers).body(Gson().toJson(paste))
+        val bufferedContentReader = paste.content.byteInputStream().bufferedReader()
+
+        val responseBody = StreamingResponseBody { outputStream: OutputStream ->
+            var numberOfBytesToWrite: Int
+            val data = CharArray(1024)
+            while ((bufferedContentReader.read(data, 0, data.size)
+                    .also { numberOfBytesToWrite = it }) != -1
+            ) {
+                outputStream.write(String(data).encodeToByteArray(), 0, numberOfBytesToWrite)
+            }
+            bufferedContentReader.close()
+        }
+
+        return ResponseEntity.ok().headers(headers).body(responseBody)
     }
 }

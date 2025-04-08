e310c2ea1e1b7a19a7df8e44a5155e6a97fc785a
Loudbook <loudbooked@gmail.com>
2025-04-07 23:06:56 -0400

    Remove href

--- frontend/src/components/Content.svelte
+++ frontend/src/components/Content.svelte
@@ -239,7 +239,6 @@
               on:click={clickNumber}
               tabindex="0"
               on:keydown={() => {}}
-              href={"#"}
             >
               <number
                 class="number"

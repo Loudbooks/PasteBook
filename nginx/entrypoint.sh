555626d02bd406740b67c0bec518e04096f7abe2
Loudbook <loudbooked@gmail.com>
2025-04-24 18:47:23 -0400

    Merge sed

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,10 +3,10 @@
 export BACKEND_HOST="${BACKEND_HOST:-backend:8080}"
 export FRONTEND_HOST="${FRONTEND_HOST:-frontend:3000}"
 
-sed "s|\${BACKEND_HOST}|${BACKEND_HOST}|g" \
-    /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+touch /etc/nginx/conf.d/default.conf
 
-sed "s|\${FRONTEND_HOST}|${FRONTEND_HOST}|g" \
+sed -e "s|\${BACKEND_HOST}|${BACKEND_HOST}|g" \
+    -e "s|\${FRONTEND_HOST}|${FRONTEND_HOST}|g" \
     /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"

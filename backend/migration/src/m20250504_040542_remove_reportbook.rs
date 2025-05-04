8ef8d40f2a36b6d7a73194ae8a315782237f7e00
Loudbook <loudbooked@gmail.com>
2025-05-04 00:13:15 -0400

    Fix migration

--- backend/migration/src/m20250504_040542_remove_reportbook.rs
+++ backend/migration/src/m20250504_040542_remove_reportbook.rs
@@ -7,80 +7,35 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .create_table(
-                Table::create()
-                    .table(User::Table)
-                    .if_not_exists()
-                    .col(ColumnDef::new(User::Ip).string().not_null().primary_key())
-                    .col(ColumnDef::new(User::Id).string().not_null())
-                    .col(ColumnDef::new(User::Requests).big_unsigned().not_null())
-                    .col(ColumnDef::new(User::CreatedAt).big_integer().not_null())
-                    .col(ColumnDef::new(User::Banned).boolean().not_null())
-                    .to_owned(),
-            )
-            .await?;
-
-        manager
-            .create_table(
-                Table::create()
+            .alter_table(
+                Table::alter()
                     .table(Paste::Table)
-                    .if_not_exists()
-                    .col(ColumnDef::new(Paste::Id).string().not_null().primary_key())
-                    .col(ColumnDef::new(Paste::Title).string().not_null())
-                    .col(ColumnDef::new(Paste::Created).big_unsigned().not_null())
-                    .col(ColumnDef::new(Paste::Wrap).boolean().not_null())
-                    .col(ColumnDef::new(Paste::CreatorIp).string().not_null())
-                    .col(ColumnDef::new(Paste::ExpiresAt).big_unsigned().not_null())
-                    .to_owned(),
-            )
-            .await?;
-
-        manager
-            .create_table(
-                Table::create()
-                    .table(PasteContent::Table)
-                    .if_not_exists()
-                    .col(ColumnDef::new(PasteContent::Id).string().not_null().primary_key())
-                    .col(ColumnDef::new(PasteContent::Content).text().not_null())
+                    .drop_column(Paste::ReportBook)
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager.drop_table(Table::drop().table(Paste::Table).to_owned()).await?;
-        manager.drop_table(Table::drop().table(User::Table).to_owned()).await?;
-        manager.drop_table(Table::drop().table(PasteContent::Table).to_owned()).await
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Paste::Table)
+                    .add_column(
+                        ColumnDef::new(Paste::ReportBook)
+                            .boolean()
+                            .not_null()
+                            .default(false),
+                    )
+                    .to_owned(),
+            )
+            .await
     }
-}
-
-#[derive(Iden)]
-#[iden(rename = "user_metadata")]
-enum User {
-    Table,
-    Ip,
-    Id,
-    Requests,
-    CreatedAt,
-    Banned,
 }
 
 #[derive(Iden)]
 #[iden(rename = "paste_metadata")]
 enum Paste {
     Table,
-    Id,
-    Title,
-    Created,
-    Wrap,
-    CreatorIp,
-    ExpiresAt,
-}
-
-#[derive(Iden)]
-#[iden(rename = "paste_content")]
-enum PasteContent {
-    Table,
-    Id,
-    Content,
+    ReportBook,
 }
